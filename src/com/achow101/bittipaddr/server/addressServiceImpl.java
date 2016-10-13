978d6b0175122146d7c424b7ad39dfda6bea02f8
Andrew Chow <achow101@gmail.com>
2016-10-12 23:44:12 -0400

    Get addresses from DynamoDB
    Connects to Amazon Web Services DynamoDB to retrieve address data for an id.

--- src/com/achow101/bittipaddr/server/addressServiceImpl.java
+++ src/com/achow101/bittipaddr/server/addressServiceImpl.java
@@ -7,6 +7,17 @@
 
 package com.achow101.bittipaddr.server;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import javax.servlet.ServletException;
@@ -16,6 +27,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Andy Chow on 10/12/2016.
@@ -29,12 +42,45 @@ public class addressServiceImpl extends HttpServlet {
         String requestURI = request.getRequestURI();
         String id = requestURI.substring(requestURI.lastIndexOf("/") + 1);
 
+        // Setup the aws dynamo db client
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        // Lookup ID and get current address, increment index
+        String address = "";
+        Table table = dynamoDB.getTable("Bittipaddrs");
+        try {
+            Item item = table.getItem("ID", id);
+            int currAddrInx = item.getInt("AddrIndex");
+            List<String> addresses = item.getList("Addresses");
+            if(currAddrInx < addresses.size()) {
+                address = addresses.get(currAddrInx);
+                currAddrInx++;
+
+                // Update index in DB
+                UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                        .withPrimaryKey("ID", id)
+                        .withUpdateExpression("set AddrIndex=:i")
+                        .withValueMap(new ValueMap()
+                                .withNumber(":i", currAddrInx));
+                table.updateItem(updateItemSpec);
+
+                // TODO: Check with blockcypher that it isn't used. Do in a Do-While loop
+            }
+            else {
+                address = addresses.get(addresses.size() - 1);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error in getting item.");
+        }
+
         // Set response content type
         response.setContentType("text/html");
 
         // Actual logic goes here.
         PrintWriter out = response.getWriter();
-        out.println("16YzX277NztZNm7YUfAq4FyTJBuPNu8xte");
+        out.println("<a href=bitcoin:" + address + ">" + address + "</a>");
     }
 
 }
